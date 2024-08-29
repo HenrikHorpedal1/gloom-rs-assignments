edbe1022e6f33be1fbd31c6b207d5841d63ccb34
HenrikHorpedal <hhorpedal@gmail.com>
2024-08-29 15:38:06 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -67,9 +67,13 @@ unsafe fn create_vao(vertices: &Vec<f32>, indices: &Vec<u32>) -> u32 {
     
     let mut bufferIDs: u32 = 0;
     gl::GenBuffers(n, &mut bufferIDs);
-    gl::BindBuffer(gl::ARRAYBUFFer,bufferIDs);
+    gl::BindBuffer(gl::ARRAY_BUFFER,bufferIDs);
 
     let f32_numbers: [f32; 5] = [2.0, 2.0, 3.0, 4.0, 5.0];
+
+    let size_of_array = byte_size_of_array(&f32_numbers);
+
+    gl::BufferData(gl::ARRAY_BUFFER, size_of_array, &mut f32_numbers, gl::STATIC_DRAW) 
     // * Generate a VBO and bind it
     // * Fill it with data
     // * Configure a VAP for the data and enable it
@@ -77,7 +81,7 @@ unsafe fn create_vao(vertices: &Vec<f32>, indices: &Vec<u32>) -> u32 {
     // * Fill it with data
     // * Return the ID of the VAO
 
-    0
+    0    
 }
 
 
