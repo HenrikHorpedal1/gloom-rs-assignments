418883407bbd50efbfde5aed233dd6846c1eea2d
HenrikHorpedal <hhorpedal@gmail.com>
2024-09-01 21:08:45 +0200

    add a fix

--- src/main.rs
+++ src/main.rs
@@ -171,7 +171,7 @@ fn main() {
 
 
         let vertex = vec![-0.6,-0.6,0.0,0.6,-0.6,0.0,0.0,0.6,0.0];
-        let indices = vec![0,1,2,3,4,5,6,7,8];
+        let indices = vec![0,1,2];
         let my_vao = unsafe { create_vao(&vertex, &indices) };
 
 
@@ -192,7 +192,6 @@ fn main() {
                 .link()
                 .activate()
         };
-
 
         unsafe {
             gl::BindVertexArray(my_vao);
