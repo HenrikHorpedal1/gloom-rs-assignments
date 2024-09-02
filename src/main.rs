4ac8bbe93f3fa96604133ffee9ca3c70ad95fe91
HenrikHorpedal <hhorpedal@gmail.com>
2024-09-02 11:41:17 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -226,7 +226,7 @@ fn main() {
         //let righ_angled = create_2d_triangle_vertices(TriangleType::RightAngled,(-0.8,0.8), 0.4);
         //vertex_array.extend(righ_angled);
 
-        let equalateral = create_2d_triangle_vertices(TriangleType::Equilateral,(-0.8,0.1), 0.4);
+        let equalateral = create_2d_triangle_vertices(TriangleType::Equilateral,(-0.1,0.1), 0.3);
         vertex_array.extend(equalateral);
 
         //let indices = (0..number_of_triangles*3).collect();
